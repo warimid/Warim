656bcc08b97b89b795dcf44ac07a5a8850696267
WarimID <warim.id@gmail.com>
2016-11-18 20:13:42 +0700

    Update warim.js

--- warim.js
+++ warim.js
@@ -44,3 +44,26 @@ var HW_config = {
     translations: { title: "Hot News" }
   };
 (function() { var ch = document.createElement('script'); ch.type = 'text/javascript'; ch.async = true; ch.src = '//cdn.headwayapp.co/widget.js'; var sc = document.getElementsByTagName('script')[0]; sc.parentNode.insertBefore(ch, sc); })();
+
+ var appended = false,
+  bookmark = document.createElement("div");
+  bookmark.id = "subscribeOnscroll";
+  bookmark.innerHTML = '<div id="site-subs" class="slideUpbox">\
+  <div id="mc_embed_signup">\
+    <form action="//kompiajaib.us11.list-manage.com/subscribe/post?u=c592732e727cadd0c18aa2e53&amp;id=67606d3fc9" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>\
+      <div id="mc_embed_signup_scroll">\
+        <label for="mce-EMAIL">SUBSCRIBE <span class="to">to</span> <span class="our">Our News Letters</span><br/>\
+        <span class="get">Get hottest posts into your inbox</span></label>\
+        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>\
+        <div style="position: absolute; left: -5000px;" aria-hidden="true">\
+          <input type="text" name="b_c592732e727cadd0c18aa2e53_67606d3fc9" tabindex="-1" value="">\
+        </div>\
+        <div class="clear"></div>\
+          <input type="submit" value="Subscribe" onclick="hidesubscribebox()" name="subscribe" id="mc-embedded-subscribe" class="button">\
+      </div>\
+    </form>\
+  </div>\
+  <div class="close-shareOnscroll" onclick="hidesubscribe()">&#215;</div>\
+</div>\
+';
+function hidesubscribe(){document.getElementById("subscribeOnscroll").style.display="none"}function hidesubscribebox(){document.getElementById("subscribeOnscroll").style.display="none",createCookie("hideDialog","hide",7000)}function createCookie(e,o,n){if(n){var t=new Date;t.setTime(t.getTime()+24*n*60*60*1e3);var i="; expires="+t.toGMTString()}else var i="";document.cookie=e+"="+o+i+"; path=/"}function readCookie(e){for(var o=e+"=",n=document.cookie.split(";"),t=0;t<n.length;t++){for(var i=n[t];" "==i.charAt(0);)i=i.substring(1,i.length);if(0==i.indexOf(o))return i.substring(o.length,i.length)}return null}function eraseCookie(e){createCookie(e,"",-1)}onscroll=function(){var e=document.documentElement.scrollTop||document.body.scrollTop;e>800&&(appended||(document.body.appendChild(bookmark),appended=!0),readCookie("hideDialog")&&(document.getElementById("subscribeOnscroll").style.display="none"))};
